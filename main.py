40d0c1092c2b1f5ed64ae530e23f8d4a999522a3
Jack <jacknovak.jn@gmail.com>
2023-05-10 21:37:44 +0100

    updates, nearly there

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def add_time(start, time, day=''):
     hours = int(split_start[0]) + int(split_length[0])
     minutes = int(minutes_ampm[0]) + int(split_length[1])
 
+    extra_hours = minutes // 60
+    for x in range(extra_hours):
+        hours += 1
+        minutes -= 60
+
     twelves = hours // 12
     for clock in range(twelves):
         hours -= 12
@@ -14,21 +19,20 @@ def add_time(start, time, day=''):
     ampm = minutes_ampm[1]
     extra_days = twelves // 2
 
-    extra_hours = minutes // 60
-    for x in range(extra_hours):
-        hours += 1
-        minutes -= 60
-
 
 
 
     day_list = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     new_day = ''
     comment = ''
+    # if minutes > 10:
+    #     minutes = minutes.zfill(2)
 
 
     if ampm == 'AM':
-        if twelves % 2 == 0:
+        if int(split_start[0]) < 12 and hours >= 12:
+            ampm = 'PM'
+        elif twelves % 2 == 0:
             ampm = 'AM'
         else:
             ampm = 'PM'
@@ -52,26 +56,34 @@ def add_time(start, time, day=''):
                     new_day_index -= 7
 
 
+
+
             new_day = day_list[new_day_index]
             # print(new_day)
             # return f"{hours}:{minutes} {ampm},{new_day} {comment}"
-            print(f"{hours}:{minutes} {ampm},{new_day} {comment}")
+            print(f"{hours}:{minutes:02d} {ampm}, {new_day} {comment}")
 
-        # return f"{hours}:{minutes} {ampm} {comment}"
-        print(f"{hours}:{minutes} {ampm} {comment}")
+        else:
+            # return f"{hours}:{minutes} {ampm} {comment}"
+            print(f"{hours}:{minutes:02d} {ampm} {comment}")
+
+
 
 
     elif ampm == 'PM':
-        if not twelves % 2:
+        if twelves % 2 == 1:
              ampm = 'AM'
         else:
             ampm = 'PM'
 
-        if twelves <= 2:
+
+
+
+        if twelves == 1 or twelves == 2 :
             comment = "(next day)"
         elif twelves >= 3:
             extra_days = (twelves // 2) + 1
-            comment =f"({extra_days} days later)"
+            comment = f"({extra_days} days later)"
 
         if day:
             weekday = day.title()
@@ -87,12 +99,12 @@ def add_time(start, time, day=''):
 
             new_day = day_list[new_day_index]
             # return f"{hours}:{minutes} {ampm},{new_day} {comment}"
-            print(f"{hours}:{minutes} {ampm},{new_day} {comment}")
+            print(f"{hours}:{minutes:02d} {ampm}, {new_day} {comment}")
 
-        # return f"{hours}:{minutes} {ampm} {comment}"
-        print(f"{hours}:{minutes} {ampm} {comment}")
-
-
+        else:
+            print(f"{hours}:{minutes:02d} {ampm} {comment}")
 
 
-add_time("1:00 AM", "36:00")
+
+
+add_time("6:30 PM", "205:12")
